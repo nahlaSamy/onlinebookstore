912617cbd3fe3eec33df28ef5f9adcb26f7a7342
Nahla samy <Nahlasamy583@gmail.com>
2024-06-12 01:03:46 +0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './App.css'
+import AuthLayout from './modules/SharedModule/components/AuthLayout/AuthLayout'
+import NotFound from './modules/SharedModule/components/NotFound/NotFound'
+import Login from './modules/AuthModule/components/Login/Login'
+import Register from './modules/AuthModule/components/Register/Register'
+import ForgetPass from './modules/AuthModule/components/ForgetPass/ForgetPass'
+import ResetPass from './modules/AuthModule/components/ResetPass/ResetPass'
+import MasterLayout from './modules/SharedModule/components/MasterLayout/MasterLayout'
+import LandingPage from './modules/MasterModule/components/LandingPage/LandingPage'
+import BooksList from './modules/MasterModule/components/Cart/BooksList/BooksList'
+import Cart from './modules/MasterModule/components/Cart/Cart'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { jwtDecode } from 'jwt-decode'
+import { useState } from 'react'
+import ProtectedRoute from './modules/SharedModule/components/ProtectedRoute/ProtectedRoute'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [userData, setUserData] = useState(null);
+
+  const saveUserData = () => {
+      let encodedToken = localStorage.getItem('userToken');
+      if (encodedToken) {
+          let decodedToken = jwtDecode(encodedToken);
+          setUserData(decodedToken);
+          console.log(decodedToken);
+      } else {
+          console.error("No token found");
+      }
+  };
+  const routes = createBrowserRouter([
+    {
+      path: '/',
+      element: <AuthLayout />,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <Login saveUserData={saveUserData} /> },
+        { path: 'login', element: <Login saveUserData={saveUserData}/> },
+        { path: 'register', element: <Register /> },
+        { path: 'forget-pass', element: <ForgetPass /> },
+        { path: 'reset-pass', element: <ResetPass /> },
+      ]
+    },
+    {
+      path: 'dashboard',
+      element:  <ProtectedRoute userData={userData}> <MasterLayout /></ProtectedRoute>,
+      errorElement: <NotFound />,
+      children: [
+        { index: true, element: <LandingPage /> },
+        { path: 'home', element: <LandingPage /> },
+        { path: 'books-list', element: <BooksList /> },
+        { path: 'cart', element: <Cart /> },
+      ]
+    },
+  ])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <RouterProvider router={routes}/>
+    <ToastContainer/>
+
     </>
   )
 }
