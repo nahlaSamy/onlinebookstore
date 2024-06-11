68211aa4f9b77954483faac583eb202ce9ed0f48
Nahla samy <Nahlasamy583@gmail.com>
2024-06-12 01:09:15 +0300

    folders structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
