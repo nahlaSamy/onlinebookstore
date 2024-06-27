fc786348096dcd5125ef6594bcdb8b15667fd9ce
Nahla samy <Nahlasamy583@gmail.com>
2024-06-27 23:22:24 +0300

    updates

--- src/modules/MasterModule/components/LandingPage/LandingPage.tsx
+++ src/modules/MasterModule/components/LandingPage/LandingPage.tsx
@@ -1,7 +1,55 @@
-import React from 'react'
-
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import ChangePass from '../../../AuthModule/components/ChangePass/ChangePass';
+import Footer from '../../../SharedModule/components/Footer/Footer';
+import NavBar from '../../../SharedModule/components/NavBar/NavBar';
+import HeroSection from '../HeroSection/HeroSection';
+import FeaturedBook from '../../FeaturedBook/FeaturedBook';
+import NewRelease from '../NewRelease/NewRelease';
+import Categories from '../Categories/Categories';
+import BookSale from '../BookSale/BookSale';
+import LatestArticles from '../LatestArticles/LatestArticles';
 export default function LandingPage() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <div>LandingPage</div>
-  )
+    <div>
+      <div className="">
+        <NavBar/>
+      </div>
+      <div className='bg-info '>
+        <HeroSection/>
+      </div >
+      <div>
+        <Categories/>
+      </div>
+      <div className=" ">
+        <NewRelease/>
+      </div>
+    
+      <div className="">
+      <FeaturedBook/>
+
+      </div>
+      {/* <button className='btn btn-info' onClick={handleShow}>Change Password</button> */}
+       <div className='p-5'>
+        <BookSale/>
+       </div>
+       <div className=''>
+
+       <LatestArticles/>
+       </div>
+      <div className=''>
+        <Footer/>
+      </div>
+      <Modal size="xl" show={show} onHide={handleClose}>
+        <Modal.Body>
+          <ChangePass handleClose={handleClose} />
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
---
+++ src/modules/SharedModule/components/NavBar/NavBar.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from "../../../../assets/images/sample logo 1.png";
+import './NavBar.css'; 
+import { useNavigate } from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
+import ChangePass from '../../../AuthModule/components/ChangePass/ChangePass';
+
+export default function NavBar() {
+    let navigate = useNavigate()
+    const handleLogout = () => {
+        localStorage.removeItem('userToken'); 
+        navigate('/login')
+        console.log("Logout clicked"); 
+    };
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                {/* <Navbar.Brand href="#home"><img src={logo} className='rounded rounded-5 bg-info img-fluid' alt="" /></Navbar.Brand> */}
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home" className="nav-link-with-divider">Home</Nav.Link>
+                        <Nav.Link href="#link" className="nav-link-with-divider">About Us</Nav.Link>
+                        <Nav.Link href="#link" className="nav-link-with-divider">Books</Nav.Link>
+                        <Nav.Link href="#link" className="nav-link-with-divider">New Release</Nav.Link>
+                        <Nav.Link href="#link" className="nav-link-with-divider">Contact Us</Nav.Link>
+                        <Nav.Link href="#link">Blog</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        <NavDropdown
+                            title={<i className="fa-solid fa-user fa-lg mx-2"></i>}
+                            id="basic-nav-dropdown"
+                            className='me-auto'
+                        >
+                            <NavDropdown.Item href="#" onClick={handleShow}> <i className="fa-solid fa-key fa-xs mx-2"></i>Change Password</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item onClick={handleLogout}> <i className="fa-solid fa-right-from-bracket fa-xs mx-2"></i>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link> <i className="fas fa-shopping-bag fa-lg mx-2"></i></Nav.Link>
+                        <Nav.Link> <i className="fa-regular fa-heart fa-lg mx-2"></i></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+         <Modal size="xl" show={show} onHide={handleClose}>
+         <Modal.Body>
+           <ChangePass handleClose={handleClose} />
+         </Modal.Body>
+       </Modal>
+        </>
+    )
+}
